9c3f6bbd0993bb58ea600eb6c3837c4fbd21c89b
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2025-12-16 21:38:47 -0700

    Update product.cs
    added a way to generate product id. Prefix is based on type of product

--- models/product.cs
+++ models/product.cs
@@ -1,11 +1,41 @@
+using System;
+using System.Collections.Generic;
+
 public class Product
 {
+    public static readonly Dictionary<ProductCategory, int> _categoryCounters = new Dictionary<ProductCategory, int>();
+
+    public string ProductId { get; private set; }
     public string Name { get; set; }
     public int Quantity { get; set; }
+    public ProductCategory Category { get; }
 
-    public Product( string name, int quantity )
+    public Product(string name, int quantity, ProductCategory category)
     {
         Name = name;
         Quantity = quantity;
+        Category = category;
+        GenerateProductId();
     }
-}
+
+    private void GenerateProductId()
+    {
+        if (!_categoryCounters.ContainsKey(Category))
+        {
+            _categoryCounters[Category] = 0;
+        }
+
+        _categoryCounters[Category]++;
+
+        string prefix = Category switch
+        {
+            ProductCategory.Technology => "T",
+            ProductCategory.Beauty => "B",
+            ProductCategory.HomeAppliance => "H",
+            _ => "U"
+        };
+
+        ProductId = $"{prefix}{_categoryCounters[Category]:D4}";
+    }
+
+ }
